c0039f3f2c9f9c6bda77693f52ab92c029c2fa43
Tapu Mandal <online.tapu@gmail.com>
2020-10-30 15:39:30 +0600

    Navigation done

--- src/main/java/com/tapumandal/ecommerce/domain/appnavigation/NavigationServiceImpl.java
+++ src/main/java/com/tapumandal/ecommerce/domain/appnavigation/NavigationServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -51,8 +52,13 @@ public class NavigationServiceImpl implements NavigationService {
     }
 
     @Override
-    public Navigation getNavigation() {
-        return navigationRepository.getNavigation();
+    public List<MenuList> getNavigation() {
+
+        Navigation navigation =  navigationRepository.getNavigation();
+        List<MenuList> menuLists = new Gson().fromJson(navigation.getNavigation(), List.class);
+
+
+        return menuLists;
     }
 
     @Override
